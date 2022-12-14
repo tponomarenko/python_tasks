4e8d1546d9d6ccacc79daab404634f06055966d0
Tetiana Ponomarenko <tane4ka377@gmail.com>
2022-12-14 18:42:12 +0000

    add default city

--- weather/script.py
+++ weather/script.py
@@ -3,7 +3,7 @@ import requests
 
 
 host_url = "https://wttr.in/"
-city_name = os.getenv("CITY")
+city_name = os.getenv("CITY", default="Chernihiv")
 response = requests.get(f"{host_url}{city_name}")
 if response.status_code != 200:
     print("error occured")
