6d15a9d7f4a7860abe838e885421782257b2ae63
Kateryna Shaforostova <katrin.shaphorotova@gmail.com>
2022-12-23 17:23:02 +0000

    Add language support

--- weather/script.py
+++ weather/script.py
@@ -1,10 +1,17 @@
+
 import os
 import requests
 
 
 host_url = "https://wttr.in/"
 city_name = os.getenv("CITY", default="Chernihiv")
-response = requests.get(f"{host_url}{city_name}")
+language = os.getenv("LANG", default="uk")
+
+response = requests.get(
+    f"{host_url}{city_name}",
+    headers={"Accept-Language": language}
+)
+
 if response.status_code != 200:
     print("error occured")
 else:
